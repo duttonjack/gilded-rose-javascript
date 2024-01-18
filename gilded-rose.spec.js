942d8bf5407cba9c217857e82e7e9c610048be42
Jack Dutton <duttonjack@gmail.com>
2024-01-18 15:53:01 -0500

    implemented one test to understand functinality

--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -1,14 +1,23 @@
 import { expect, describe, it } from "vitest";
 import { Item, items, updateQuality } from "./gilded-rose.js";
 
+
+// Example Test
 describe("updateQuality", () => {
   it("reduces quality and sellIn of basic items by 1", () => {
     const testItem = new Item("basic", 5, 3);
     items.push(testItem);
-
     updateQuality();
-
     expect(testItem.quality).toBe(2);
     expect(testItem.sellIn).toBe(4);
+  })
+  it("reduces quality and sellIn of basic items by 1", () => {
+    const testItem = new Item("basic", 3, 3);
+    items.push(testItem);
+    updateQuality();
+    expect(testItem.quality).toBe(2);
+    expect(testItem.sellIn).toBe(2);
+
+  
   });
 });
