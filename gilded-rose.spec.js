3a78bad7ba771b48ef7ee9e52519d1418c4de1fc
Jack Dutton <duttonjack@gmail.com>
2024-01-18 17:19:51 -0500

    test suite completed for all rules

--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -34,7 +34,56 @@ describe("updateQuality", () => {
     expect(testItem.quality).toBe(48);
     expect(testItem.sellIn).toBe(-4);
   });
-  
+  it("Aged Brie increases in value as it gets older", () => {
+    const testItem = new Item("Aged Brie", 8, 23);
+    items.push(testItem);
+    updateQuality();
+    updateQuality();
+    updateQuality();
+    expect(testItem.quality).toBe(26);
+    expect(testItem.sellIn).toBe(5);
+  });
+  it("Sulfuras never changes in sellIn or Quality", () => {
+    const testItem = new Item("Sulfuras, Hand of Ragnaros", 100, 250);
+    items.push(testItem);
+    updateQuality();
+    updateQuality();
+    updateQuality();
+    expect(testItem.quality).toBe(250);
+    expect(testItem.sellIn).toBe(100);
+  });
+  it("Backstage item increases in quality as SellIn decreases", () => {
+    const testItem = new Item("Backstage passes to a TAFKAL80ETC concert", 13, 16);
+    items.push(testItem);
+    updateQuality();
+    expect(testItem.quality).toBe(17);
+    expect(testItem.sellIn).toBe(12);
+  });
+  it("Backstage item increases in quality as SellIn decreases", () => {
+    const testItem = new Item("Backstage passes to a TAFKAL80ETC concert", 8, 20);
+    items.push(testItem);
+    updateQuality();
+    expect(testItem.quality).toBe(22);
+    expect(testItem.sellIn).toBe(7);
+  });
+  it("Backstage item increases in quality as SellIn decreases", () => {
+    const testItem = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 12);
+    items.push(testItem);
+    updateQuality();
+    updateQuality();
+    updateQuality();
+    expect(testItem.quality).toBe(20);
+    expect(testItem.sellIn).toBe(3);
+  });
+  it("Backstage item quality goes to 0 after the concert", () => {
+    const testItem = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 200);
+    items.push(testItem);
+    updateQuality();
+    updateQuality();
+    updateQuality();
+    expect(testItem.quality).toBe(0);
+    expect(testItem.sellIn).toBe(-2);
+  });
 });
 
 
