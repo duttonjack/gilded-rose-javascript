5540041d9c499dac01ff1b14511243aac3ce744c
Jack Dutton <duttonjack@gmail.com>
2024-01-18 16:44:28 -0500

    implemented tests for core rules

--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -2,22 +2,39 @@ import { expect, describe, it } from "vitest";
 import { Item, items, updateQuality } from "./gilded-rose.js";
 
 
+
 // Example Test
 describe("updateQuality", () => {
   it("reduces quality and sellIn of basic items by 1", () => {
-    const testItem = new Item("basic", 5, 3);
+    const testItem = new Item("basic", 5, 53);
     items.push(testItem);
     updateQuality();
-    expect(testItem.quality).toBe(2);
+    expect(testItem.quality).toBe(52);
     expect(testItem.sellIn).toBe(4);
+    
   })
-  it("reduces quality and sellIn of basic items by 1", () => {
-    const testItem = new Item("basic", 3, 3);
+  it("quality of an item can never be negative", () => {
+    const testItem = new Item("basic", 16, 0);
     items.push(testItem);
     updateQuality();
-    expect(testItem.quality).toBe(2);
-    expect(testItem.sellIn).toBe(2);
-
-  
+    expect(testItem.quality).toBe(0);
+    expect(testItem.sellIn).toBe(15);
   });
+  it("quality of an item can never be > 50", () => {
+    const testItem = new Item("Aged Brie", 16, 50);
+    items.push(testItem);
+    updateQuality();
+    expect(testItem.quality).toBe(50);
+    expect(testItem.sellIn).toBe(15);
+  });
+  it("When sellIn < 0, quality degrades twice as fast", () => {
+    const testItem = new Item("basic", -3, 50);
+    items.push(testItem);
+    updateQuality();
+    expect(testItem.quality).toBe(48);
+    expect(testItem.sellIn).toBe(-4);
+  });
+  
 });
+
+
